f5e622186c86d1f5cfda356255dbadb299ef9db0
minling <mzhao725@gmail.com>
2015-06-08 18:27:43 -0400

    count elements

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,80 @@
-def count_elements(array)
-  # code goes here
-end
+
+
+    # # def count_elements(array)
+    # #   hash = Hash.new
+    # #   array.each_with_index do |element, index|
+    # #     hash[element] = element.count 
+    # #   end
+    # #   hash
+    # # end
  
+# # def count_elements(array)
+# #     hash= Hash.new
+# #    array.each do |array|
+# #     hash[array] += 1 
+# #     return hash
+# #     end
+# # end
+
+# # # array = ['cat', 'dog', 'fish', 'fish']
+
+def count_elements(animals)
+  animal_hash = {}
+  animals.each do |animal|  
+    animals.count(animal)
+    animal_hash[animal] = animals.count(animal)
+  end
+  animal_hash
+end
+
+# def each(array)
+#   yield
+#   array
+# end
+
+# # [1, 2, 3].each do |element|
+# #   puts element
+
+# # end
+
+# # array = ['a', 'b', 'c']
+
+# # class Array
+
+#   def each_with_index(array)
+#     x = 0
+#     until x == array.length
+#       yield array[x], x
+#       x += 1 
+#     end    
+#   end
+
+# # # when we get to x == 3, stop
+
+# # ['a', 'b', 'c'].each_with_index do |element, index|
+# #   puts "Jeff" + "#{element}" + "#{index}"
+# # end
+
+
+
+# def times(number)
+#   x=0 
+#   until x == number
+#     yield "hello"
+#     x += 1 
+#   end
+# end
+
+# # it should yield the same amount of times as the number
+
+# times(2) do |foo|
+#   puts 'hello' + "#{foo}"
+# end
+
+
+
+
+
+
+
+
--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,8 +1,11 @@
 describe '#count_elements' do
 
   it 'should return a hash with the count of each element in the simple array' do
-    array = ['cat', 'dog', 'fish', 'fish']
-    hash = count_elements(array)
+    hash = count_elements(['cat', 'dog', 'fish', 'fish'])
+
+  
+    
+
     expect(hash['cat']).to eq(1)
     expect(hash['dog']).to eq(1)
     expect(hash['fish']).to eq(2)
